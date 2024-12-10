5158791413afa3cecf6d5e877dabcb448770691d
David E. Perez Negron R <p1r0@nethunters.xyz>
2024-12-09 22:14:55 -0300

    done 2.2: Creating Our Runtime

--- src/system.rs
+++ src/system.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, ops::Add};
 
 /// This is the System Pallet.
 /// It handles low level state needed for your blockchain.
@@ -8,10 +8,53 @@ pub struct Pallet {
 }
 
 impl Pallet {
+
 	pub fn new() -> Self {
         Self {
             block_number: 0,
             nonce:BTreeMap::new()
         }
 	}
+
+    pub fn block_number(&self) -> u32 {
+        self.block_number
+    }
+
+    pub fn inc_block_number(&mut self) {
+        //crashes if overflow for purpose
+        self.block_number = self.block_number.checked_add(1).unwrap();
+    }
+
+    pub fn inc_nonce(&mut self, who: &String) {
+        let nonce = self.nonce.get(who).unwrap_or(&0);
+        self.nonce.insert(who.clone(), nonce + 1);
+    }
+    pub fn get_nonce(&self, who: &String) -> u32 {
+        *self.nonce.get(who).unwrap_or(&0)
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn init_system(){
+        let system = super::Pallet::new();
+        assert_eq!(system.block_number(), 0);
+    }
+
+    #[test]
+    fn inc_block_number(){
+        let mut system = super::Pallet::new();
+        system.inc_block_number();
+        assert_eq!(system.block_number, 1);
+    }
+
+    #[test]
+    fn inc_nonce(){
+        let alice = String::from("alice");
+        let mut system = super::Pallet::new();
+        system.inc_nonce(&alice.clone());
+        assert_eq!(system.get_nonce(&alice.clone()), 1);
+    }
 }
